14598569a724206c797f06d89c275ac6064d1643
konnatoad <severi.moisare@gmail.com>
2025-07-18 03:22:51 +0300

    Did some tweaking on the bootsrap

--- install.sh
+++ install.sh
@@ -14,15 +14,6 @@ chezmoi init github.com/konnatoad/dotfiles || error "Failed to init chezmoi"
 # install starship
 curl -sS https://starship.rs/install.sh | sh
 
-# Install Rust (if missing)
-if ! command -v cargo &>/dev/null; then
-  curl https://sh.rustup.rs -sSf | sh || error "Failed to install Rust"
-fi
-
-# Source Rust env
-source "$HOME/.cargo/env"
-source ~/.zprofile
-
 # Apply dotfiles
 chezmoi apply -R || error "Failed to apply dotfiles"
 
@@ -32,33 +23,29 @@ yes | sudo pacman -S --needed --noconfirm - <~/.local/share/chezmoi/setup/pacman
 # Install AUR packages
 yes | yay -S --needed --noconfirm --answerdiff=None --answerclean=None - <~/.local/share/chezmoi/setup/aur_packages.txt
 
-# oh-my-zsh and plugins
+# install ohmyzsh and plugins
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  echo "Installing oh-my-zsh..."
+  echo "Getting ohmyz.sh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ""
 else
-  echo "oh-my-zsh already present, skipping."
+  echo ".oh-my-zsh already found, skipping."
 fi
 
-ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
-
-if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+  echo "Getting zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 else
   echo "zsh-autosuggestions already present, skipping."
 fi
 
-if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+  echo "Getting zsh-syntax-highlighting"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 else
   echo "zsh-syntax-highlighting already present, skipping."
 fi
 
-# Set Zsh as the default shell
-if [[ "$SHELL" != "/usr/bin/zsh" ]]; then
-  chsh -s /usr/bin/zsh || error "Failed to set Zsh as default shell"
-else
-  echo "Zsh already set as default shell."
-fi
+# set zsh as shell
+chsh -s /usr/bin/zsh
 
 echo "All done."
